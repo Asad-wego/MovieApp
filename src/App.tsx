98c3728ca2093eda047d8ebe2fa4ed58b6dcd90e
Asad-wego <asad@wego.com>
2024-03-11 23:05:26 +0500

    Project is created and run on android and ios it's fine to load a simple component

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+/*
+ * Created by Asad on 11 Mar 2024
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Movie App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
