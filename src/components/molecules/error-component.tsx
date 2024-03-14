d60eb5a671bc889e8126d6782ce9311aa9d5c638
Asad-wego <asad@wego.com>
2024-03-14 20:38:50 +0500

    Implemented Back Button Option in Error Component, as per Suggestion

--- src/components/molecules/error-component.tsx
+++ src/components/molecules/error-component.tsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  I18nManager,
+} from 'react-native';
 import {TextView, CustomButton} from '../atoms';
 import label from '../../translations/en.json';
+import R from '../../styles';
 
 interface ErrorProps {
   message: string;
   buttonText?: string;
   onButtonPress?: () => void;
+  showBack?: boolean;
+  onBackPress?: () => void;
 }
 
 const ErrorComponent: React.FC<ErrorProps> = ({
   message,
   buttonText = label.try_again,
   onButtonPress,
+  showBack = false,
+  onBackPress,
 }) => {
+  const handleBackPress = () => {
+    if (onBackPress) {
+      onBackPress();
+    }
+  };
+
   return (
     <View style={styles.container}>
+      {showBack && (
+        <TouchableOpacity style={styles.backButton} onPress={handleBackPress}>
+          <Image
+            style={[
+              styles.backIcon,
+              {
+                transform: [{scaleX: I18nManager.isRTL ? -1 : 1}],
+              },
+            ]}
+            source={R.images.crossIcon}
+          />
+        </TouchableOpacity>
+      )}
       <TextView style={styles.message}>{message}</TextView>
       {onButtonPress && (
         <CustomButton
@@ -39,6 +69,16 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    zIndex: 1,
+  },
+  backIcon: {
+    width: 24,
+    height: 24,
   },
 });
 
