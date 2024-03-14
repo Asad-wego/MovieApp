25cbfe13b8859486e3ae8b9bbbf7c0d609aa46c2
Asad-wego <asad@wego.com>
2024-03-14 22:16:36 +0500

    SC-5 Enhanced Home-Screen UI with Reusable Components, Network Integration, and Redux
    Ticket No=SC-5
    Ticket Link= https://github.com/users/AsadNajum/projects/2/views/3?pane=issue&itemId=56102729

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,16 +1,74 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, SafeAreaView} from 'react-native';
-import routes from '../navigations/routes';
+import React, {useEffect} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+import axios, {CancelTokenSource} from 'axios';
+import {RootState} from '../redux/store';
+import {SearchInput} from '../components/atoms';
+import {LoadingComponent, ErrorComponent} from '../components/molecules';
+import {MovieItem} from '../components/molecules';
+import {useAppDispatch, useAppSelector} from '../redux/hooks';
+import {fetchMovies} from '../network/movie-api';
+import Debouncer from '../utils/debouncer';
+import Routes from '../navigations/routes';
+import label from '../translations/en.json'; //TODO: will apply translation later
 
 const HomeScreen: React.FC = ({navigation}) => {
+  const dispatch = useAppDispatch();
+  const {isLoading, movies, error} = useAppSelector(
+    (state: RootState) => state.movies,
+  );
+  const debouncer = new Debouncer(300);
+  const cancelTokenSource: CancelTokenSource = axios.CancelToken.source();
+
+  const cleanUp = () => {
+    // TODO: cancel ongoing requests
+    cancelTokenSource.cancel();
+    debouncer.cleanup;
+  };
+
+  useEffect(() => {
+    dispatch(fetchMovies(cancelTokenSource.token, ''));
+    return () => cleanUp();
+  }, [dispatch]);
+
+  const onTextChange = (text: string) =>
+    debouncer.debounce(() =>
+      dispatch(fetchMovies(cancelTokenSource.token, text)),
+    );
+
+  const retryFetchData = () =>
+    dispatch(fetchMovies(cancelTokenSource.token, ''));
+
+  const renderItem = ({item}: {item: Movie}) => (
+    <MovieItem
+      movie={item}
+      onPress={() =>
+        navigation.navigate(Routes.detailsScreen, {movieId: item['#IMDB_ID']})
+      }
+    />
+  );
+
   return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate(routes.detailsScreen)}>
-        <Text>details</Text>
-      </TouchableOpacity>
-      <Text>home</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <SearchInput
+        placeholder={label.search_movies}
+        onChangeText={(text: string) => onTextChange(text)}
+      />
+      {isLoading && <LoadingComponent />}
+      {error ? (
+        <ErrorComponent
+          message={error}
+          buttonText={label.try_again}
+          onButtonPress={retryFetchData}
+        />
+      ) : (
+        <FlatList
+          data={movies}
+          renderItem={renderItem}
+          keyExtractor={item => item['#IMDB_ID']}
+          contentContainerStyle={{flexGrow: 1}}
+        />
+      )}
+    </View>
   );
 };
 
